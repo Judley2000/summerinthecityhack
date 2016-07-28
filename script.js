1ca7514aef3e7a9844c8d2faff7df6d6436c7052
judley2000 <judley.l2000@gmail.com>
2016-07-28 17:09:27 +0000

    olaf game

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    $("#howto").click(function(){
+        
+        $("#howtoinfo").toggle();
+        
+        
+        
+    
+    })
+    
+    
+    
+    
+    
+    
+    
+})
+
+
